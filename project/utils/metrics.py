011bbcdf1153cd0f7cc327b411f92e1a1be7639d
astatarinov <temedio2000@gmail.com>
2024-04-14 17:56:55 +0300

    changes

--- project/utils/metrics.py
+++ project/utils/metrics.py
@@ -20,6 +20,64 @@ def _calculate_rate_share(
     return period_share
 
 
+def calculate_add_margin(
+    prediction: float,
+    target: float,
+    cbr_key_rate: float,
+    profit_rate_diff: float = 0.5,
+    on_deposit_rate_diff: float = -0.9,
+    on_loan_rate_diff: float = 1.0,
+) -> float:
+    """
+    Calculates additional margin that can be earned by using model
+    """
+    # if deficit predicted than we do nothing 
+    if prediction <= 0:
+        return 0
+    deviation = prediction - target
+    add_earnings = (
+        (
+            # gi from derivatives
+            prediction * _calculate_rate_share(cbr_key_rate, profit_rate_diff)
+        ) 
+        - (
+            # gi that would be earned by OverNight deposit anyway
+            target * _calculate_rate_share(cbr_key_rate, on_deposit_rate_diff) 
+        )
+        - (
+            # prediction mistake "fee"
+            # if pred > target > 0, need to loan ON to cover difference
+            max(deviation, 0) * _calculate_rate_share(cbr_key_rate, on_loan_rate_diff)
+        )
+    )
+    return add_earnings
+
+
+def calculate_total_add_margin(
+    predictions: Iterable[float],
+    targets: Iterable[float],
+    cbr_key_rates: Iterable[float],
+    profit_rate_diff: float = 0.5,
+    on_deposit_rate_diff: float = -0.9,
+    on_loan_rate_diff: float = 1.0,
+) -> float:
+    """
+    Calculate total additional profit over the period
+    """
+    earnings = sum(
+        target * calculate_add_margin(
+            pred,
+            target,
+            key_rate,
+            profit_rate_diff,
+            on_deposit_rate_diff,
+            on_loan_rate_diff
+        )
+        for pred, target, key_rate in zip(predictions, targets, cbr_key_rates)
+    )
+    return earnings
+
+
 def calculate_daily_profit(
     prediction: float,
     target: float,
@@ -44,7 +102,7 @@ def calculate_daily_profit(
 def calculate_total_earnings(
     predictions: Iterable[float],
     targets: Iterable[float],
-    cbr_key_rate: float,
+    cbr_key_rates: Iterable[float],
     profit_rate_diff: float = 0.5,
     on_deposit_rate_diff: float = -0.9,
     on_loan_rate_diff: float = 1.0,
@@ -56,19 +114,19 @@ def calculate_total_earnings(
         target * calculate_daily_profit(
             pred,
             target,
-            cbr_key_rate,
+            key_rate,
             profit_rate_diff,
             on_deposit_rate_diff,
             on_loan_rate_diff
         )
-        for pred, target in zip(predictions, targets)
+        for pred, target, key_rate in zip(predictions, targets, cbr_key_rates)
     )
     return earnings
 
 
 def check_business_requirements(
-    predictions: pd.Series | np.array,
-    targets: pd.Series | np.array,
+    predictions: pd.Series | np.ndarray,
+    targets: pd.Series | np.ndarray,
     max_deviation: float = 0.42,
 ) -> bool:
     """

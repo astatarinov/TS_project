18e06730a809b5b03020c3b6417875ec701b873a
Stepan Denisov <sd.denisoff@gmail.com>
2024-04-14 17:06:43 +0300

    add tsfresh demo notebook

--- run.py
+++ run.py
@@ -12,7 +12,6 @@ from project.pipeline import detect_change_point, run_model_pipeline
 if __name__ == "__main__":
     print(f'Start serving at {datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
-    # schedule.every().second.do(run_model_pipeline)  # for testing
     schedule.every().day.at('07:00').do(run_model_pipeline)
     schedule.every().hour.at(':00').do(detect_change_point)
 
